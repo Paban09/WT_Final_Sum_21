f483754f8c41f9520fd7e3efc7b7eb7292c96870
Paban09 <masudranapaban22@gmail.com>
2021-07-14 04:17:56 +0600

    Assignment

---
+++ Assignment/Views/main_footer.php
@@ -0,0 +1,6 @@
+<html>
+    <head></head>
+	<footer>
+	    <h3 align="center">Webtech Assignment</h3>
+	</footer>
+</html>
---
+++ Assignment/Views/main_header.php
@@ -0,0 +1,8 @@
+<html>
+    <head></head>
+	<body>
+	    <div align = "center">
+		    <h3>Assignment</h3>
+		</div>
+	</body>
+</html>
